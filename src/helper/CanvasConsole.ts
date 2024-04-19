73763098cea6573aeb71852bd4a7df74c695f1c7
atoz <523333404@qq.com>
2024-04-19 23:42:32 +0800

    💐 canvas绘制类

---
+++ src/helper/CanvasConsole.ts
@@ -0,0 +1,52 @@
+export class CanvasConsole implements CANVAS_CONSOLE {
+  readonly canvas = document.createElement('canvas')
+  readonly context
+  readonly components: Array<CANVAS_COMPONENT> = []
+  dpr = devicePixelRatio
+  rpx: number
+  constructor(
+    public width: number,
+    public height: number
+  ) {
+    this.canvas.width = width * devicePixelRatio
+    this.canvas.height = width * devicePixelRatio
+    this.rpx = (width * devicePixelRatio) / 375
+    this.context = this.canvas.getContext('2d')!
+
+    this.draw()
+  }
+
+  /**
+   * 重绘画布
+   */
+  private draw() {
+    const { context } = this
+    context.clear()
+
+    this.components.forEach((c) => {
+      context.beginPath()
+      context.save()
+      c.handle(this)
+      context.restore()
+    })
+
+    requestAnimationFrame(() => this.draw())
+  }
+
+  /**
+   * 添加组件
+   * @param c
+   */
+  push(c: CANVAS_COMPONENT) {
+    this.components.push(c)
+    this.components.sort((a, b) => (a.order ?? 0) - (b.order ?? 0))
+  }
+
+  /**
+   * 添加到父级节点
+   * @param element
+   */
+  appendTo(element: HTMLElement) {
+    element.appendChild(this.canvas)
+  }
+}
