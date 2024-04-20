a88649cf996d875c74526ee4f5b88c1276535f9d
atoz <523333404@qq.com>
2024-04-20 23:21:37 +0800

    🐻 添加演示操作

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,49 @@
 import './extend/CanvasRenderingContext2D'
 import { CanvasConsole } from './helper/CanvasConsole'
 import { MiddleImage } from './helper/component/MiddleImage'
-
 import sourceSrc from './assets/w.png'
-import sourceSrc2 from './assets/h.webp'
 
 const canvasConsole = new CanvasConsole(800, 800)
-
 const withImage = new MiddleImage(canvasConsole, sourceSrc)
 
 canvasConsole.push(withImage)
-canvasConsole.appendTo(document.body)
+canvasConsole.appendTo(document.querySelector('.main .win')!)
+
+/** 演示操作 */
+const btns = document.querySelectorAll('.btns button')!
+const upLocalBtn = btns.item(0)
+const retateBtn = btns.item(1)
+
+// 上传图片
+upLocalBtn.addEventListener('click', function () {
+  const input = document.createElement('input')
+  input.type = 'file'
+  input.accept = 'image/*'
+  input.onchange = function () {
+    const files = input.files!
+    const [file] = files
+    const url = URL.createObjectURL(file)
+    withImage.switchImage(url)
+  }
+  input.click()
+})
+
+// 旋转
+let animeIng = false
+let r = 0
+retateBtn.addEventListener('click', function () {
+  if (animeIng) {
+    return void 0
+  }
+
+  // 动画开始
+  animeIng = true
+  withImage
+    .anime(600, (scale) => {
+      withImage.setAgent(r + 90 * scale)
+    })
+    .then(() => {
+      r += 90
+      animeIng = false
+    })
+})
