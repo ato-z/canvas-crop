cb85e30aa8b45fd6d56e76a26d6c586873a72d33
ato-z <523333404@qq.com>
2024-04-22 22:50:42 +0800

    🐻 绘制虚线方法

--- src/extend/CanvasRenderingContext2D.ts
+++ src/extend/CanvasRenderingContext2D.ts
@@ -2,9 +2,6 @@
  * 对canvas 2d环境方法进行扩展
  */
 
-const c = document.createElement('canvas')
-const ctx = c.getContext('2d')!
-
 CanvasRenderingContext2D.prototype.clear = function () {
   this.clearRect(0, 0, this.canvas.width, this.canvas.height)
 }
@@ -13,4 +10,33 @@ CanvasRenderingContext2D.prototype.drawImageByDpr = function (
   ...args: Parameters<CanvasRenderingContext2D['drawImage']>
 ) {
   this.drawImage(...args)
+}
+
+CanvasRenderingContext2D.prototype.drawDashedRect = function (
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+  dashLength: number,
+  cornerRadius = 0
+) {
+  this.setLineDash([dashLength, dashLength]) // 设置虚线样式
+  this.beginPath()
+  this.moveTo(x + cornerRadius, y)
+  this.lineTo(x + width - cornerRadius, y)
+  this.arcTo(x + width, y, x + width, y + cornerRadius, cornerRadius)
+  this.lineTo(x + width, y + height - cornerRadius)
+  this.arcTo(
+    x + width,
+    y + height,
+    x + width - cornerRadius,
+    y + height,
+    cornerRadius
+  )
+  this.lineTo(x + cornerRadius, y + height)
+  this.arcTo(x, y + height, x, y + height - cornerRadius, cornerRadius)
+  this.lineTo(x, y + cornerRadius)
+  this.arcTo(x, y, x + cornerRadius, y, cornerRadius)
+  this.closePath()
+  this.stroke()
 }
